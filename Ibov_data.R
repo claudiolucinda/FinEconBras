986e184d7c9be61d40d3a7c403a10cf4940f7950
Claudio Lucinda <claudiolucinda@usp.br>
2018-02-28 12:17:50 -0300

    Additional Codes

--- Ibov_data.R
+++ Ibov_data.R
@@ -3,12 +3,17 @@
 # Claudio R. Lucinda
 #########################################################
 install.packages("quantmod")
+install.packages("BatchGetSymbols")
+install.packages("tidyquant")
+library(BatchGetSymbols)
 library(PortfolioAnalytics)
 library(xlsx)
 library(tidyverse)
 library(quantmod)
+library(tidyquant)
 
 rm(list=ls())
+graphics.off()
 # Importando os dados
 IBOV_Data<-read.xlsx("./Comp_IBOV.xlsx",sheetName="IBOV",stringsAsFactors=F)
 tickers<-data.frame(IBOV_Data$Ticker,stringsAsFactors=FALSE)
@@ -23,27 +28,73 @@ datafinal<-"2018-02-23"
 # SAPR11
 # TAEE11
 # VVAR11
-getSymbols(tickers_Yah[1:35],auto.assign=TRUE,from=datainicial,to=datafinal)
-getSymbols(tickers_Yah[37:52],auto.assign=TRUE,from=datainicial,to=datafinal)
-getSymbols(tickers_Yah[54:56],auto.assign=TRUE,from=datainicial,to=datafinal)
-getSymbols(tickers_Yah[58:62],auto.assign=TRUE,from=datainicial,to=datafinal)
-getSymbols(tickers_Yah[64],auto.assign=TRUE,from=datainicial,to=datafinal)
+# dataEnv <- new.env()
+# getSymbols(tickers_Yah[1:35],auto.assign=TRUE,from=datainicial,to=datafinal,env=dataEnv)
+# getSymbols(tickers_Yah[37:52],auto.assign=TRUE,from=datainicial,to=datafinal,env=dataEnv)
+# getSymbols(tickers_Yah[54:56],auto.assign=TRUE,from=datainicial,to=datafinal,env=dataEnv)
+# getSymbols(tickers_Yah[58:62],auto.assign=TRUE,from=datainicial,to=datafinal,env=dataEnv)
+# getSymbols(tickers_Yah[64],auto.assign=TRUE,from=datainicial,to=datafinal,env=dataEnv)
+# plist <- eapply(dataEnv, Ad)
+# IBOV_Cots <- do.call(merge, plist)
 
 tickers_Yah_fin<-tickers_Yah[!tickers_Yah %in% c("KLBN11.SA","SAPR11.SA","TAEE11.SA","VVAR11.SA")]
 
-###############################################
-# Funções para automatizar as coisas
-###############################################
 
-gen_ret <- function(.xts_frame) {
-  y<-assign(.xts_frame)
-  x<-Return.calculate(y[,6])[-1,]
+##############################################
+# Versão Marcelo Perlin - 100%
+##############################################
+
+df_IBOV<-BatchGetSymbols(tickers_Yah,first.date = datainicial,last.date = datafinal,thresh.bad.data = .75)
+data_IBOV<-reshape.wide(df_IBOV$df.tickers)$price.close
+
+conv_ts <- function(.df) {
+  OUT<-xts(.df[,-1],order.by=.df[,1])
+  return(OUT)
 }
 
-Returns<-map_df(tickers_Yah_fin,gen_ret)
-
-for (.df in tickers_Yah_fin) {
-  cat(.df)
-  x<-gen_ret(.df)
-  names(x)<-paste0("ret_",.df)
+clean_na <- function(DF) {
+  OUT<-DF[, sapply(DF, function(x) sum(is.na(x)))!=nrow(DF)]
+  
+  return(OUT)
 }
+
+remove_outliers <- function(x, .pct=.25, iqr.tresh=1.5, na.rm=TRUE, ...) {
+  .pctsup<-1-.pct
+  qnt <- quantile(x, probs=c(.pct, .pctsup), na.rm = na.rm, ...)
+  H <- iqr.tresh * IQR(x, na.rm = na.rm)
+  y <- x
+  y[x < (qnt[1] - H)] <- NA
+  y[x > (qnt[2] + H)] <- NA
+  y
+}
+
+df_returns <- function(.df) {
+  df_temp<-.df
+  for (nom in colnames(.df)) {
+    df_temp[,nom]<-Return.calculate(df_temp[,nom])
+    
+  }
+  return(df_temp)
+}
+
+df_monthly <- function(.df) {
+  df_0<-to.monthly(.df[,1])[,4]
+  colnames(df_0)[1]<-colnames(.df)[1]
+  len<-length(colnames(.df))
+  for (nom in seq(2,len)) {
+    df_0<-merge(df_0,to.monthly(.df[,nom])[,4])
+    colnames(df_0)[nom]<-colnames(.df)[nom]
+  }
+  return(df_0)
+}
+
+data_IBOV<-conv_ts(data_IBOV)
+data_IBOV<-remove_outliers(data_IBOV,pct=.25,iqr.tresh = 3)
+data_IBOV<-clean_na(data_IBOV)
+
+
+IBOV_Returns<-df_returns(data_IBOV)
+data_IBOV_monthly<-df_monthly(data_IBOV)
+
+IBOV_Returns_monthly<-df_returns(data_IBOV_monthly)
+IBOV_Returns_Final<-na.omit(IBOV_Returns_monthly)
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # FinEconBras
-Códigos para o Curso de Finanças II na USP
+Códigos para o Curso de Finanças II na USP. Arquivos:
+* `Initial_Code.R` - Código para uma análise inicial, usando o pacote `PortfolioAnalytics`
+* `Portfolio_Analysis.R`- Código para análise de carteira, também usando o pacote `PortfolioAnalytics`
+* `Ibov_data.R`- Código para baixar os dados das ações componentes do IBOVESPA, usando o mesmo pacote `PortfolioAnalytics` e `BatchGetQuotes` do [Marcelo Perlin](https://msperlin.github.io/)
